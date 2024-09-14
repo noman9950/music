54300036cfab3e36d21778169eb1a56fcb35765f
Noman9950 <nomanshahzad9950@gmail.com>
2024-09-14 22:08:46 +0500

    update home

--- src/components/UpNext.jsx
+++ src/components/UpNext.jsx
@@ -27,12 +27,33 @@ const { Title, Text } = Typography;
 
 const UpNext = () => (
   <div>
-    <Title level={3} className="text-white mb-4" style={{color:'white'}}>Up Next</Title>
-    <List
+    <Title level={3} className="text-white mb-4" style={{ color: 'white' }}>Up Next</Title>
+
+    <List style={{
+      overflow: 'auto',
+      '&::-webkit-scrollbar': {
+        display: 'none', // Hide the scrollbar for WebKit browsers (Chrome, Safari, Edge, etc.)
+    },
+      height: '62vh'
+    }}
       itemLayout="horizontal"
       dataSource={[
         {
           title: 'Blah black qut',
+          artist: 'Drake',
+          duration: '3:17 min',
+          genre: 'EDM music',
+          image: musicImage,
+        },
+        {
+          title: 'Blah black qut55',
+          artist: 'Drake',
+          duration: '3:17 min',
+          genre: 'EDM music',
+          image: musicImage,
+        },
+        {
+          title: 'Blah black qut2',
           artist: 'Drake',
           duration: '3:17 min',
           genre: 'EDM music',
@@ -48,28 +69,36 @@ const UpNext = () => (
         // Add more tracks here
       ]}
       renderItem={item => (
-        <List.Item className="border-none mt-2 rounded-xl" style={{backgroundColor:'#080808'}}>
-          <List.Item.Meta 
+        <List.Item className="border-none mt-2 rounded-xl" style={{ backgroundColor: '#080808' }}>
+          <List.Item.Meta
             avatar={
-              <div style={{width:90,height:95}} className='ml-3'>
-              <img src={item.image} alt={item.title} className="w-full h-full rounded-lg" />
+              <div style={{ width: 90, height: 95 }} className='ml-3'>
+                <img src={item.image} alt={item.title} className="w-full h-full rounded-lg" />
               </div>
-              }
-            title={<Text className="text-white" style={{color:'white'}}>{item.title}</Text>}
+            }
+            title={<Text className="text-white" style={{ color: 'white' }}>{item.title}</Text>}
             description={
               <div>
-                <Text className="text-gray-400" style={{color:'white'}}>{item.artist} • {item.duration}</Text>
+                <Text className="text-gray-400" style={{ color: 'white' }}>{item.artist} • {item.duration}</Text>
                 <br />
-                <br/>
-                <Text className="text-gray-500" style={{color:'white'}}>{item.genre}</Text>
+                <br />
+                <Text className="text-gray-500" style={{ color: 'white' }}>{item.genre}</Text>
               </div>
             }
           />
-          <CloseOutlined className="text-gray-400 mr-3" style={{marginTop:-65}} />
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: 45
+          }}>
+            <CloseOutlined className="text-gray-400 mr-3" />
+            <PlusOutlined className="text-gray-400 mr-3" />
+          </div>
         </List.Item>
       )}
     />
-    <Input.Search placeholder="Add genres & playlists" className="mt-4 bg-gray-700 border-gray-600 text-white rounded-full"/>
+    <Input.Search placeholder="Add genres & playlists" className="mt-4 bg-gray-700 border-gray-600 text-white rounded-full" />
     <div className="flex flex-wrap gap-2 mt-4">
       <Button type="primary" className="bg-purple-600 border-none rounded-full">Random</Button>
       <Button className="bg-gray-700 text-gray-300 border-none rounded-full">Rap</Button>
